0200d46aa198be1bfc19edb641569f45cc96cffc
akash <akash@akash>
2020-08-17 02:19:29 +0530

    Initial Commit

---
+++ basic-auth/src/test/java/basicauth/BasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package basicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
